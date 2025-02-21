e149e5cf59912e7c547571ab696011ee17e5056f
taylor-musso <taylormusso04@gmail.com>
2025-02-20 16:24:42 -0800

    tests passing

--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,16 @@
 // The parse() function uses Ohm to produce a match object for a given
-// source code program, using the grammar in the file carlos.ohm.
+// source code program, using the grammar in the file SnackScript.ohm.
 
-import * as fs from "node:fs"
-import * as ohm from "ohm-js"
+import * as fs from "node:fs";
+import * as ohm from "ohm-js";
 
-const grammar = ohm.grammar(fs.readFileSync("src/SnackScript.ohm"))
+const grammar = ohm.grammar(fs.readFileSync("src/SnackScript.ohm"));
 
 // Returns the Ohm match if successful, otherwise throws an error
 export default function parse(sourceCode) {
-  const match = grammar.match(sourceCode)
-  if (!match.succeeded()) throw new Error(match.message)
-  return match
+  const match = grammar.match(sourceCode);
+  if (match.failed()) {
+    throw match.message;
+  }
+  return match;
 }
